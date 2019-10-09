2bd399d2e0ba5aec96712007da2574401c51da0c
faziz <faisal.aziz@gmail.com>
2019-10-09 23:41:53 +1100

    Added a test for Rotation abstraction.

---
+++ src/test/java/com/faziz/assignment/sim/RotationTest.java
@@ -0,0 +1,70 @@
+package com.faziz.assignment.sim;
+
+import static com.faziz.assignment.sim.Direction.*;
+import static com.faziz.assignment.sim.Rotation.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RotationTest {
+
+    @Test
+    public void testRotate_NORTH() {
+        Direction direction = NORTH;
+        Direction expResult = WEST;
+        Rotation rotation = LEFT;
+        Direction result = rotation.rotate(direction);
+        assertEquals(expResult, result);
+        
+        direction = NORTH;
+        expResult = EAST;
+        rotation = RIGHT;
+        result = rotation.rotate(direction);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testRotate_WEST() {
+        Direction direction = WEST;
+        Direction expResult = SOUTH;
+        Rotation rotation = LEFT;
+        Direction result = rotation.rotate(direction);
+        assertEquals(expResult, result);
+        
+        direction = WEST;
+        expResult = NORTH;
+        rotation = RIGHT;
+        result = rotation.rotate(direction);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testRotate_SOUTH() {
+        Direction direction = SOUTH;
+        Direction expResult = WEST;
+        Rotation rotation = LEFT;
+        Direction result = rotation.rotate(direction);
+        assertEquals(expResult, result);
+        
+        direction = SOUTH;
+        expResult = EAST;
+        rotation = RIGHT;
+        result = rotation.rotate(direction);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testRotate_EAST() {
+        Direction direction = EAST;
+        Direction expResult = NORTH;
+        Rotation rotation = LEFT;
+        Direction result = rotation.rotate(direction);
+        assertEquals(expResult, result);
+        
+        direction = EAST;
+        expResult = SOUTH;
+        rotation = RIGHT;
+        result = rotation.rotate(direction);
+        assertEquals(expResult, result);
+    }
+    
+}
