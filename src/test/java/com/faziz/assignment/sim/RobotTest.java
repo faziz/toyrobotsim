a3187dbb4aed638eaeff69324e5f3cd9a0dbfd13
faziz <faisal.aziz@gmail.com>
2019-10-10 14:06:59 +1100

    Setting up Robot abstraction.

--- src/test/java/com/faziz/assignment/sim/RobotTest.java
+++ src/test/java/com/faziz/assignment/sim/RobotTest.java
@@ -34,9 +34,12 @@ public class RobotTest {
      */
     @Test
     public void testMove() {
+        robot = new Robot(NORTH, tableTop);
         Boolean expResult = TRUE;
         Boolean result = robot.move();
         assertEquals(expResult, result);
+        assertEquals(0, tableTop.getCurrentPosition().x);
+        assertEquals(1, tableTop.getCurrentPosition().y);
     }
 
     /**
@@ -44,9 +47,9 @@ public class RobotTest {
      */
     @Test
     public void testGetDirection() {
-        Direction expResult = null;
-        Direction result = robot.getDirection();
-        assertEquals(expResult, result);
+        robot = new Robot(NORTH, tableTop);
+        Direction direction = robot.rotate(RIGHT);
+        assertEquals(EAST, direction);
     }
     
 }
