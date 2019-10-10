3087f68e1fb79450e9eeabc95c1d6e0cb37ddefc
faziz <faisal.aziz@gmail.com>
2019-10-10 19:25:54 +1100

    Passing the commands through the file.

--- src/main/java/com/faziz/assignment/sim/ToyRobotSimulator.java
+++ src/main/java/com/faziz/assignment/sim/ToyRobotSimulator.java
@@ -1,10 +1,14 @@
 package com.faziz.assignment.sim;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import static java.lang.String.format;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.function.Function;
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Stream.of;
 
 /**
@@ -32,16 +36,42 @@ public class ToyRobotSimulator {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ToyRobotSimulator sim = new ToyRobotSimulator();
-        Scanner sc = new Scanner(System.in);
-        
-        //Command processing loop.
-        while (sc.hasNextLine()) {
-            String cmdLine = sc.nextLine();
-            String cmdName = cmdLine.startsWith("PLACE ") ? "PLACE" : cmdLine;
-            sim.cmdHandlers.getOrDefault(cmdName, sim::unsupportedCommand).apply(cmdLine);
+        //Load robot commands and run each of them one-by-one.
+        sim.robotCommands().forEach(sim::handleCommand);
+    }
+    
+    /**
+     * @return Returns list of commands for the robot.
+     * @throws IOException 
+     */
+    private List<String> robotCommands() throws IOException {
+        try (BufferedReader reader = loadReader()) {
+            return reader.lines().
+                map(cmd -> cmd.trim()).
+                    filter(cmd -> !cmd.isEmpty()).
+                        collect(toList());
         }
+    }
+
+    /**
+     * @return Reader based on the command file on the classpath.
+     */
+    private BufferedReader loadReader() {
+        return new BufferedReader(
+            new InputStreamReader(
+                this.getClass().getResourceAsStream("/robot-commands.txt")));
+    }
+    
+    /**
+     * Handles commands one-by-one
+     * @param cmd 
+     */
+    private void handleCommand(final String cmd) {
+        String cmdName = cmd.startsWith("PLACE ") ? "PLACE" : cmd;
+        //If a command handler is found process the command, else execute unsupportedCommandHandler.
+        this.cmdHandlers.getOrDefault(cmdName, this::unsupportedCommandHandler).apply(cmd);
     }
     
     /**
@@ -132,7 +162,7 @@ public class ToyRobotSimulator {
      * @param cmd
      * @return 
      */
-    private Boolean unsupportedCommand(String cmd) {
+    private Boolean unsupportedCommandHandler(String cmd) {
         System.err.println("Command not supported.");
         return false;
     }
